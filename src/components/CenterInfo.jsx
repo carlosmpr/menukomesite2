e71612d75d4f7b1c93c1bc2d88d77c9817949329
carlosmpr <carlosmpr0508@gmail.com>
2021-10-08 11:03:44 -0400

    Gif Positioning

--- src/components/CenterInfo.jsx
+++ src/components/CenterInfo.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import Iphone from '../assets/icons/full.svg'
 export default function CenterInfo() {
     return (
-        <div className="w-screen h-screen flex flex-col items-center justify-center gap-y-4 ">
+        <div className=" relative w-screen h-screen flex flex-col items-center justify-center gap-y-4 ">
         <h1 className="text-4xl font-bold ">Use The Amazon Recommendations Secret <span className="text-primary">to sell more  </span></h1>
         <h3 className="text-2xl font-regular">Side dishes, Drinks , Similar plates and more</h3>
         <img src={Iphone} alt={Iphone} className="w-1/4"/>
+        <div className="absolute   bg-red-400" style={{left:'42.2%', top:'30%', width:'15.6%', height:"55%"}}></div>
     </div>
     )
 }
--- src/components/Reasubles/SideInfo.jsx
+++ src/components/Reasubles/SideInfo.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 export default function SideInfo({ text, image, reverse }) {
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center bg-gray-100">
+    <div className=" w-screen h-screen flex flex-col items-center justify-center bg-gray-100">
+    
       <div className={`w-full ${reverse} flex p-4 items-center justify-center`}>
         <div className="w-2/5">
           <h3 className="text-3xl text-center font-semibold">{text}</h3>
         </div>
-        <div className="w-2/5">
+        <div className="w-2/5 relative">
+            <div className="absolute w-6/12 h-4/5 bg-red-400" style={{left:'15%', top:'10%'}}></div>
           <img src={image} alt={image} className="w-4/5" />
         </div>
       </div>
